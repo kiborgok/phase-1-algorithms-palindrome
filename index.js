42105ccef0e146ce49b9acefcd77db5f6661621a
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-16 11:24:04 +0300

    implement isPalindrome func

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word).toLowerCase()
+  if (word.toLowerCase() === reversedWord) {
+    return true
+  }
+  return false
+}
+
+function reverseString(str) {
+  const strArray = str.split("")
+  const reversedArray = strArray.reverse()
+
+  const joinToStr = reversedArray.join("")
+
+  return joinToStr
 }
 
 /* 
   Add your pseudocode here
+  check if "word".toLowerCase() === reversed("word").toLowerCase()
+  if true
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  So isPalindrome func takes in a string then returns true if the
+  reverse of the string === the string
 */
 
 // You can run `node index.js` to view these console logs
